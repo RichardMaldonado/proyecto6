a1355f1711a8fb99b9e893471110d2e5b2ebfd49
laguila <laguila3467@gmail.com>
2015-11-16 11:36:31 -0300

    hola

---
+++ proyec6/src/proyec6/Proyec6.java
@@ -0,0 +1,17 @@
+
+package proyec6;
+
+/**
+ *
+ * @author 17034147-3
+ */
+public class Proyec6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
